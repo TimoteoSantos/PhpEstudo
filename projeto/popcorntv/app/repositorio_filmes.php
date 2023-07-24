7068ea4982455c503e11aba92c68790feb32d246
timoteo santos <santostimoteo21@gmail.com>
2023-07-24 15:13:58 -0300

    continuando repositorio

--- projeto/popcorntv/app/repositorio_filmes.php
+++ projeto/popcorntv/app/repositorio_filmes.php
@@ -15,4 +15,29 @@ class RepositorioFilmesMySQL implements IRepositorioFilmes
 	//atributo que guardara a conexao com o banco de dados
 	private $conexao;
 
+	//construtor do repositorio filmes
+	public function __construct()
+	{
+		//cria o objeto que sera responsavel pelas chamadas ao banco de dados
+		$this->conexao = new Conexao("localhos","root","","popcorntv");
+		//conecata ao banco de dados
+		if($this->conexao->conectar() == false){
+			
+			echo "erro" .mysqli_error();
+		}
+	}
+
+	//cadastrar um novo filme,
+	//observar que a sql é preparada e enviada para para o banco
+	public function cadastrarFilme($filme)//estamos recebendo um objteo do tipo objeto com dados e funcoes deste objeto
+	{
+
+		$titulo = $filmes->getTitulo;
+		$sinopse = $filmes->
+		
+
+	}
+
+
+
 }
