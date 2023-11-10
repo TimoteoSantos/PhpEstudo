f4864d236802f8081bfe0010d3ca509c4aa9881f
santostimoteo21@gmail.com <santostimoteo21@gmail.com>
2023-11-10 07:15:36 -0300

    inicio DAO

---
+++ php_oo/17 - DAO/SQL.php
@@ -0,0 +1,60 @@
+<?php
+/* * DAO é uma abstracao de banco de dados * * */
+
+class Sql extends  PDO
+{
+    private $conn;
+
+    //conexao ao instanciar essa classe ja é aberta uma conexao
+    public function __construct()
+    {
+        $this->conn = new PDO("mysql:dbname=php7;host=localhost","root","");
+    }
+
+    //ASSOCIANDO OS PARAMENTROS
+    private function setParams($statment, $parameters = array())
+    {
+        //percorrndo os dados do $parameters recebidos
+        foreach ($parameters as $key => $valor)
+        {   //para cada linha chamar o metodo setParam que associa uma chave a um valor
+            $this->setParams($key,$valor);
+        }
+    }
+
+    //PARAMENTRO QUE ASSOCIA
+    private function setParam($statement,$key,$value)
+    {
+        $statement->bindParams($key,$value);
+    }
+
+    //execultar a consulta
+    public function quuery($rouQuery,$params = array())
+    {
+        //preparando a consulta
+        $stmt = $this->conn->prepare($rouQuery);
+
+        //enviando para o metodo que associa os dados
+        $this->setParams($stmt,$params);
+        //execultando no banco e retornao o resultado da consulta
+
+        //execultando
+        $stmt->execute();
+
+        //retornando a resposta do execulte
+        return $stmt;
+    }
+
+    //um metodo para usar nos selects
+    public function select($rouQuery, $params =  array())
+    {
+        //usando o metodo query para preparar a query
+
+        //execultar no metodo query pegar o retorno e fazer o facth
+        $stmt = $this->quuery($rouQuery,$params);
+
+        //retornando a reposta do query em um array associativo
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+}
---
+++ php_oo/17 - DAO/config.php
@@ -0,0 +1,13 @@
+<?php
+
+spl_autoload(function ($className)
+{
+    $filename
+
+    if (file_exists($className.".php"))
+    {
+
+    }
+
+
+});
