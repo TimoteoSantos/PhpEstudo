1997623ff467ecfdac55f1d9dbdd8b7e4a8d5a32
Timoteo Santos <santostimoteo21@gmail.com>
2025-06-12 10:34:12 -0300

    estudo de arquivos

--- livro_Php/Desenvolvendo websites com PHP - Juliano Niederaurer/Capitulo - 12/trabalhandoComArquivos.php
+++ livro_Php/Desenvolvendo websites com PHP - Juliano Niederaurer/Capitulo - 12/trabalhandoComArquivos.php
@@ -1,5 +1,14 @@
 <?php
 
-//criar arquivos em php
-$nomeArquivo = 'arquivo.txt';
+    //tenta abrir um arquivo se nao consguir cria um
+    $arquivo = fopen('arquivo.csv', 'a+');
 
+    //dados
+    $data = "DADO A SER INSERIFO \n";
+
+    //escreve em um arquivo
+    fwrite($arquivo, $data);
+    
+    //ultilizando file_put_contents()
+    file_put_contents("$arquivo","$data");
+    
